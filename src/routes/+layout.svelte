79ab7d211968b7286a9f65b23f4dad5d875e18d6
jayney <jayney@jayneyui-macbookair.whitehost-root.gmail.com.beta.tailscale.net>
2023-10-14 22:33:06 +0900

    inital commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<script lang="ts">
+    import { Nav, NavItem, NavLink } from 'sveltestrap';
+</script>
+<nav class={"navbar navbar-expand-md navbar-dark bg-dark mb-4"}>
+    <a id="testf" class="navbar-brand" href="#">Location Service</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a id="intro" class="nav-link" href="/intro"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+              <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+            </svg>
+                Intro</a>
+        </li>
+        <li class="nav-item">
+            <a id="locationui" class="nav-link" href="/location"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cursor" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z"/>
+              </svg>
+                Location</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link disabled" id="servicestatus" style="display:none" ><div class="spinner-border spinner-border-sm" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>Service healthcheck..</a>
+        </li>
+        </ul> 
+    </div>
+</nav>
+<slot>
+    
+</slot>
---
+++ src/routes/location/+page.svelte
@@ -0,0 +1,47 @@
+<div id="location-filter">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <input type="text" id="device" class="form-control" placeholder="id"/>
+            <input type="password" id="authorization" class="form-control" placeholder="device authorization code"/>
+            <input type="password" id="password" class="form-control" placeholder="password"/>
+            <div class="btn-group mr-2">
+              <button id="min10" type="button" class="btn btn-sm btn-outline-secondary" onclick="locationui.drawLocations(device.value, 10,password.value, authorization.value)">10분전 위치</button>
+              <button id="hour1" type="button" class="btn btn-sm btn-outline-secondary" onclick="locationui.drawLocations(device.value, 60, password.value, authorization.value)">1시간 전 위치</button>
+              <button id="hour3" type="button" class="btn btn-sm btn-outline-secondary" onclick="locationui.drawLocations(device.value, 180, password.value, authorization.value)">3시간 전 위치</button>
+              <button id="hour6" type="button" class="btn btn-sm btn-outline-secondary" onclick="locationui.drawLocations(device.value, 360, password.value, authorization.value)">6시간 전 위치</button>
+              <input id="datetimes" readonly type="text" fromurl="false" name="datetimes" />
+              <p id="rangeText"></p>
+            </div>
+            
+          <div class="input-group-addon">
+              <span class="glyphicon glyphicon-th"></span>
+          </div>
+          
+          <!-- <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="realtimeLocation">
+            <label class="form-check-label" for="realtimeLocation">실시간</label>
+          </div> -->
+        </div> 
+       
+      </div>
+      <div class="spinner-border" style="width: 3rem; height: 3rem; display: none; position:fixed " id="loading" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      
+</div>
+<div class="buttons">
+  <button id="share" onclick="locationui.share()" type="button" class="btn  btn-sm  btn-outline-secondary ">URL 복사하기(*주의)</button>
+  <p>주의: 신뢰할 수 있는 사람에게만 공유하세요. 신뢰하는 사람에게 링크가 공유될 시 본인과 신뢰할 수 있는 사람이 보게 되며, <b>신뢰할 수 있는 분은 또 다른 분에게 공유가 가능하기 때문에, 보내려는 분에겐 보안을 지켜달라고 말해주세요.</b></p>
+</div>
+
+<div id="map-area">
+    <div id="map"></div>
+</div>
+<div id="timetable">
+  <div id="visualization"></div>
+</div>
+<div id="detail">
+  <code id="debugmessage">
+  </code>
+</div>
